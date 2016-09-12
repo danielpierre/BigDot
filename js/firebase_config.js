75ce652764f1461d9c0683a41b516663dda6844f
danielpierre <daniel.l.pierre@gmail.com>
2016-09-11 22:24:38 -0400

    Initial commit

---
+++ js/firebase_config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+// TODO: Replace with your project's firebase credentials
+var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+};
+firebase.initializeApp(config);
